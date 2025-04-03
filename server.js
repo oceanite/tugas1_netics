dfdcf1903af520f2bfb6aea329c04bc08f387014
oceanite <dkristinginting@gmail.com>
2025-04-03 20:44:27 +0700

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+// Store the start time when the server first runs
+const startTime = new Date(); // Capture the start time
+
+app.get("/health", (req, res) => {
+  const currentTime = new Date();
+  const uptime = (currentTime - startTime) / 1000; // Calculate uptime in seconds
+
+  res.json({
+    nama: "Beluga",
+    nrp: "5025231040",
+    status: "UP", // Server status
+    timestamp: startTime.toLocaleString("id-ID", { timeZone: "Asia/Jakarta" }), // Server start time (doesn't change)
+    uptime: `${Math.floor(uptime)} detik`, // Uptime increases dynamically
+  });
+});
+
+// Start the server
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
